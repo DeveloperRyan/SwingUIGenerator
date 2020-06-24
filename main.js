aba91b3f5a71d51d78461754bfb9227275a4e683
Ryan Elliott <rselliott99@gmail.com>
2020-06-24 10:21:34 -0400

    Make button text randomized

--- main.js
+++ main.js
@@ -1,36 +1,37 @@
-let { app, BrowserWindow } = require('electron');
-const { spawn } = require('child_process');
+let { app, BrowserWindow } = require("electron");
+const { spawn } = require("child_process");
 
 let win;
 
 function createWindow() {
-    win = new BrowserWindow({
-        width: 735,
-        height: 750,
-        autoHideMenuBar: true,
-        fullscreen: true,
-        x: 0,
-        y: 0,
-        webPreferences: {
-            nodeIntegration: true
-        }
-    })
+	win = new BrowserWindow({
+		width: 735,
+		height: 750,
+		autoHideMenuBar: true,
+		fullscreen: true,
+		x: 0,
+		y: 0,
+		webPreferences: {
+			nodeIntegration: true,
+		},
+	});
 
-    win.loadFile('index.html');
+	win.loadFile("index.html");
 }
 
-
 async function takeScreenshot(numPhotos) {
-    for (let i = 0; i < numPhotos; i++) {
-        await new Promise((resolve) => setTimeout(() => {
-            spawn('python', ['screenshot.py', i.toString()]);
-            win.reload();
-            resolve();
-        }, 150));
-    }
+	for (let i = 0; i < numPhotos; i++) {
+		await new Promise((resolve) =>
+			setTimeout(() => {
+				spawn("python", ["screenshot.py", i.toString()]);
+				win.reload();
+				resolve();
+			}, 150)
+		);
+	}
 }
 
 app.whenReady().then(function () {
-    createWindow();
-    takeScreenshot(1000);
+	createWindow();
+	// takeScreenshot(1000);
 });
--- script.js
+++ script.js
@@ -14,13 +14,12 @@ function createElements() {
 }
 
 function chooseElement() {
-	let randNum = Math.floor(Math.random() * 5) + 1;
+	let randNum = Math.floor(Math.random() * 3) + 1;
 	switch (randNum) {
 		case 1:
 			let paragraph = document.createElement("p");
-			paragraph.innerHTML =
-				Math.random().toString(36).substring(2, 15) +
-				Math.random().toString(36).substring(2, 15);
+			paragraph.innerHTML = createString(Math.floor(Math.random() * 15) + 1);
+
 			return paragraph;
 		case 2:
 			let textbox = document.createElement("input");
@@ -28,16 +27,8 @@ function chooseElement() {
 			return textbox;
 		case 3:
 			let button = document.createElement("button");
-			button.innerHTML = chooseButtonName();
+			button.innerHTML = createString(Math.floor(Math.random() * 10) + 1);
 			return button;
-		case 4:
-			let radio = document.createElement("input");
-			radio.type = "radio";
-			return radio;
-		case 5:
-			let dropdown = document.createElement("input");
-			dropdown.type = "dropdown";
-			return dropdown;
 	}
 }
 
@@ -47,26 +38,13 @@ function createInterface(numSections) {
 	}
 }
 
-function chooseButtonName() {
-	const BUTTON_TEXT = [
-		"Click me",
-		"Submit",
-		"Continue",
-		"Activate",
-		"Start",
-		"Begin",
-		"Launch",
-		"Cancel",
-		"Stop",
-		"Pause",
-		"Execute",
-		"Resume",
-		"Run",
-		"Increase",
-		"Decrease",
-	];
+function createString(length) {
+	let str = "";
+	for (let i = 0; i < length; i++) {
+		str += Math.random().toString(36).substring(2, 3);
+	}
 
-	return BUTTON_TEXT[Math.floor(Math.random() * BUTTON_TEXT.length)]; // return a random text selection
+	return str;
 }
 
 createInterface(10);
